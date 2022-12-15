9a657ce1288cd0aab3627f3ce7fdc09eadf762fb
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2022-12-14 18:56:18 -0600

    obazl-mode.el: fns to parse test dsl (in testsuite/tests files)

--- bzl/obazl-mode.el
+++ bzl/obazl-mode.el
@@ -1,26 +1,235 @@
 ;; obazl minor mode
 
-(defun obazl-read-test-spec ()
+;; if (* TEST *) then
+;;    if ref file then expect_test else ocaml_test
+
+(defun emit-cmt (line bazel-file)
+  (write-region (format "# %s\n" line) nil bazel-file t))
+
+(defun four-star (lines bazel-file)
+  (write-region (format "#4 %s\n" (car lines)) nil bazel-file t)
+  (letrec ((handler (lambda (lines)
+                      ;; (write-region (format "#---- %s\n" (car lines))
+                      ;;               nil bazel-file t)
+                      (when lines
+                        (cond
+                         ((string-match " *\\*)" (car lines))
+                          ;; eospec
+                          (write-region (format "# %s\n" (car lines))
+                                        nil bazel-file t)
+                          (cdr lines))
+                         ((string-match "\s-*\\* +" (car lines))
+                          ;; end
+                          ;; (write-region (format "#X-- %s\n" (car lines))
+                          ;;               nil bazel-file t)
+                          lines)
+                         ((string-match "\s-*\\*\\* +" (car lines))
+                          ;; end
+                          ;; (write-region (format "#XX- %s\n" (car lines))
+                          ;;               nil bazel-file t)
+                          lines)
+                         ((string-match "\s-*\\*\\*\\* +" (car lines))
+                          ;; end
+                          ;; (write-region (format "#XXX %s\n" (car lines))
+                          ;;               nil bazel-file t)
+                          lines)
+                         ((string-match "\s-*\\*\\*\\*\\* +" (car lines))
+                          ;; end
+                          ;; (write-region (format "#XXX %s\n" (car lines))
+                          ;;               nil bazel-file t)
+                          lines)
+                         ;; ((string-match "\s-*\\*\\*\\*\* +" (car lines))
+                         ;;  (let ((tail
+                         ;;         (four-star lines bazel-file)))
+                         ;;    (funcall handler tail)))
+                         (t
+                          (progn
+                            (write-region (format "#4- %s\n" (car lines))
+                                          nil bazel-file t))
+                          (funcall handler (cdr lines))))))))
+    (funcall handler (cdr lines))))
+
+(defun three-star (lines bazel-file)
+  (write-region (format "#3 %s\n" (car lines)) nil bazel-file t)
+  (letrec ((handler (lambda (lines)
+                      ;; (write-region (format "#--- %s\n" (car lines))
+                      ;;               nil bazel-file t)
+                      (when lines
+                        (cond
+                         ((string-match " *\\*)" (car lines))
+                          ;; eospec
+                          (write-region (format "# %s\n" (car lines))
+                                        nil bazel-file t)
+                          (cdr lines))
+                         ((string-match "\s-*\\* +" (car lines))
+                          ;; end
+                          (write-region (format "#X-- %s\n" (car lines))
+                                        nil bazel-file t)
+                          lines)
+                         ((string-match "\s-*\\*\\* +" (car lines))
+                          ;; end
+                          (write-region (format "#XX- %s\n" (car lines))
+                                        nil bazel-file t)
+                          lines)
+                         ((string-match "\s-*\\*\\*\\* +" (car lines))
+                          ;; new three
+                          ;; (write-region (format "#XXX %s\n" (car lines))
+                          ;;               nil bazel-file t)
+                          lines)
+                         ((string-match "\s-*\\*\\*\\*\* +" (car lines))
+                          (let ((tail
+                                 (four-star lines bazel-file)))
+                            (funcall handler tail)))
+                         (t
+                          (progn
+                            (write-region (format "#3- %s\n" (car lines))
+                                          nil bazel-file t))
+                          (funcall handler (cdr lines))))))))
+    (funcall handler (cdr lines))))
+
+(defun two-star (lines bazel-file)
+  (write-region (format "#2 %s\n" (car lines)) nil bazel-file t)
+  (letrec ((handler (lambda (lines)
+                      ;; (write-region (format "#-- %s\n" (car lines))
+                      ;;               nil bazel-file t)
+                      (when lines
+                        (cond
+                         ((string-match " *\\*)" (car lines))
+                          ;; eospec
+                          (write-region (format "# %s\n" (car lines))
+                                        nil bazel-file t)
+                          (cdr lines))
+                         ((string-match " *\\*\\*\\* +" (car lines))
+                          (let ((tail
+                                 (three-star lines bazel-file)))
+                            (funcall handler tail)))
+                         ((string-match " *\\*\\* +" (car lines))
+                          ;; end of this two-star
+                          (write-region (format "#XX %s\n" (car lines))
+                                        nil bazel-file t)
+                          lines)
+                         ((string-match " *\\* +" (car lines))
+                          ;; end of this two-star
+                          (write-region (format "#X- %s\n" (car lines))
+                                        nil bazel-file t)
+                          lines)
+                         (t
+                          (progn
+                            (write-region (format "#2- %s\n" (car lines))
+                                          nil bazel-file t))
+                          (funcall handler (cdr lines))))))))
+    (funcall handler (cdr lines))))
+
+(defun one-star (lines bazel-file)
+  (write-region (format "#1 %s\n" (car lines)) nil bazel-file t)
+  (letrec ((handler (lambda (lines)
+                      ;; (write-region (format "#- %s\n" (car lines))
+                      ;;               nil bazel-file t)
+                      (when lines
+                        (cond
+                         ((string-match " *\\*)" (car lines))
+                          ;; eospec
+                          (write-region (format "# %s\n" (car lines))
+                                        nil bazel-file t)
+                          (cdr lines))
+                         ((string-match "\s-*\\*\\*\\* +" (car lines))
+                          (error "Found *** directly after *"))
+                         ((string-match " *\\*\\* +" (car lines))
+                          (let ((tail
+                                 (two-star lines bazel-file)))
+                            (funcall handler tail)))
+                         ((string-match " *\\* +" (car lines))
+                          ;; end of this one-star
+                          (write-region (format "#X %s\n" (car lines))
+                                        nil bazel-file t)
+                          lines)
+                         (t
+                          (progn
+                            (write-region (format "#1- %s\n" (car lines))
+                                          nil bazel-file t))
+                          (funcall handler (cdr lines))))))))
+    (funcall handler (cdr lines))))
+
+(defun obazl-convert-test-spec (fname bazel-file)
   "Read the (* TEST ... *) spec."
-  (interactive)
-  (lisp-mode)
+  ;; (interactive)
+  (message "converting...")
   (goto-char (point-min))
   (let ((beg (point)))
     (forward-list)
     (letrec ((dsl (buffer-substring-no-properties beg (point)))
              (dsl-lines (split-string dsl "\n"))
              (spec (read dsl)))
-      (message "dsl list?: %s" (listp spec))
-      (dolist (tok dsl-lines)
-        (message "%s\n" tok))
+      ;; (message "dsl list?: %s" (listp spec))
+      (message "dsl is string? %s" (stringp dsl))
+      (write-region (format "\n## %s\n" fname) nil bazel-file t)
+      ;; (dolist (l dsl-lines)
+      ;;   (write-region (format "# %s\n" l) nil bazel-file t))
+      (letrec ((handler (lambda (lines)
+                          (when lines
+                            (message "line: %s" (car lines))
+                            (cond
+                             ((string-match "(\\* *TEST" (car lines))
+                              (progn
+                                (emit-cmt (car lines) bazel-file)
+                                (funcall handler (cdr lines))))
+                             ((string-match " *\\* +" (car lines))
+                              (let ((tail
+                                     (one-star lines bazel-file)))
+                                (funcall handler tail)))
+                             (t
+                              (progn
+                                (write-region (format "#0 %s\n" (car lines))
+                                              nil bazel-file t))
+                              (funcall handler (cdr lines))))))))
+         (funcall handler dsl-lines))
       )))
+
+(defun obazl-convert-tests ()
+  "Convert tests in current directory."
+  (interactive)
+  ;; (message "dired cwd: %s" (dired-current-directory t))
+  ;; (message "file at point: %s" (dired-get-filename 'no-dir))
+  (letrec ((this-dir (dired-current-directory t))
+           (files-list
+            (directory-files this-dir
+                             nil ;; relative paths
+                             ".*\.ml[ily]?$"
+                             ;; assumption: no directory names match
+                             ))
+           (bazel-file (format "%sBUILD.obazl.bazel" this-dir))
+           (oldbuf (current-buffer))
+           (work-buf (generate-new-buffer "obazl")))
+    (message "this dir: %s" this-dir)
+    (save-current-buffer
+      (set-buffer work-buf)
+      (lisp-mode)
+      (write-region
+       (concat (format "## %s\n\n" this-dir)
+               ;; FIXME: dsl parser must discover needed rules
+               "load(\"//bzl/rules/test:rules.bzl\", \"repl_test\")\n")
+       nil bazel-file)
+      (dolist (f files-list)
+        (let ((relf (format "%s%s" this-dir f)))
+          (message "\nFILE: %s" relf)
+          (erase-buffer)
+          (insert-file-contents relf)
+          (goto-char (point-min))
+          (let ((first-line (thing-at-point 'line)))
+            ;; (message "line 1: %s" first-line)
+            (if (string-match "(\* *TEST" first-line)
+                (obazl-convert-test-spec f bazel-file))
+            ))))
+    )
+  )
 
 (defun obazl-gen-repl-tests () ;; (directory)
   "List the .ml files in current directory."
   (interactive) ; "DDirectory name: ")
-  (let ((ml-files-list
+  (let ((this-dir (file-name-directory buffer-file-name))
+        (ml-files-list
          (directory-files
-          (file-name-directory buffer-file-name)
+          this-dir
           nil ;; relative paths
           ".*.ml"
           ))
